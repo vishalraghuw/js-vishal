7a467131977d5cf3262013f33fb669d06a4d50fc
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-03-19 11:28:11 +0000

    we check conversion here with equility also

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = "vishal"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=>true; 0=>false
 // "" => false
@@ -23,9 +23,50 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof (stringNumber));
+// console.log(someNumber);
+// console.log(typeof (stringNumber));
 
+// ************** Operations **************
+
+let value=3;
+let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1="hello";
+let str2="vishal"
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// this types of code is not suggest what happens in this code is before string cacultaion is process what when string come its tread all as string then
+// please write this type code
+// console.log((3+4)*5%3);
+// console.log(+true); // output 1
+// console.log(+""); // output 0
+
+// console.log("2">1); // js ne atomatic 2 ko convert kar diya num me
+// console.log("2">1) // output is true
+
+// explanition writing in notebook
+// console.log(null>0);  // false;
+// console.log(null==0); // false
+// console.log(null>=0); // true 
+
+// teeno cases me false deta hai 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
 
 
 
