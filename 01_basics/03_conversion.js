305a1aa7aa83d276b1af563cd6552dd890847a7b
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-03-19 06:25:12 +0000

    conversion of data type

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,32 @@
+let score = "vishal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); /* output is number */
+// console.log(valueInNumber); /* output is NAN it is not a number 33abc is not a pure number when you convert it in js then it convert in a number but when you actully see the value in a js then it is showing NAN nan is also a type */
+
+// "33" => 33
+// "33abc" =>NAN actuallyvaluewhen prints  (but type conversion me typeof krege tab to number hi dega)
+// true =>1; false=>0
+// NULL =>0; undefined=>NAN
+// "vishal" =>NAN 
+
+let isLoggedIn = "vishal"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+// "" => false
+// "vishal" => true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof (stringNumber));
+
+
+
+
+
