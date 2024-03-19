5b58e8709adf22bf1da6bf7a20bbd51bbca0e434
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-03-19 05:46:17 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node.js, not browser browser me chal jata hai node.js me nahi alert ko use krne ka different sytax hai node ke andar
+
+console.log(3
+    +
+    3) // code readability is should be high
+
+let name = "hitesh"
+let age = 18;
+let isLoggedIn = false
+let state=null
+
+//  this is a primitive datatype
+// number => 2 to power 53 or 54 aisa kuch hai
+// bigint number bada hai 
+// string => ""
+// boolean =>true/false
+// null => standalone value hai null ek type hai datatype
+// undefined => when value is not assign
+// symbol => unique
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
