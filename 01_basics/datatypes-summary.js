bc602d42fb7a8af4f9e9a31ea9a680dd7f9aa238
vishalraghuw <91356875+vishalraghuw@users.noreply.github.com>
2024-03-20 04:58:53 +0000

    dataypes primitive non primitive

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+const heros=["saktiman","naagraj","doga"];
+let myobj={
+    myname:"vishal",
+    age:22,
+}
+const myfunction=function(){
+    console.log("hello world");
+}
+
+console.log(typeof heros);  // object
+console.log(typeof myobj); // object
+console.log(typeof myfunction); // function
